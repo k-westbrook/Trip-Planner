c8dbbdb98ccc7add3363e54fdfbd9b00ec6d36ae
k-westbrook <kcheriewestbrook@gmail.com>
2018-11-13 17:45:48 -0500

    Map added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("I'm working on Trip Planner");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidGVkZHliZWFyc29jayIsImEiOiJjam9nYmE3YWowZGJwM3hxd2J4MnRpeG92In0.x3SxwoKRgFBUH6LWrEImSQ';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
